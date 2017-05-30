b3bbf3cc231059cfaecd6eb5a6834b93086c6201
anthonyg0715 <anthonyg.0715@gmail.com>
2017-05-30 14:16:17 -0400

    Select Statement
    Comparing two ways how to use the select statement in T-SQL

---
+++ T-SQL - Select Statement.sql
@@ -0,0 +1,12 @@
+/*
+Using select * to reurn a result from table Employees
+*/
+select *
+from HR.Employees
+
+/*
+Using select to return certain attributes from the table Employees which is the more common way to do it
+rather than select *
+*/
+select lastname, firstname
+from HR.Employees
